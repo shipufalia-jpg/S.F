07afd7b38bbb801c57b7d66295a19c10ebf23714
shipufalia-jpg <shipufalia@gmail.com>
2026-05-28 18:25:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,12 +122,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # ================= UPLOAD FOLDER =================
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    UPLOAD_PATH = os.path.join(BASE_DIR, "static", "uploads", "live_media")
-
-    os.makedirs(UPLOAD_PATH, exist_ok=True)
-
+    
     # ================= LOGIN =================
     login_manager.init_app(app)
 
