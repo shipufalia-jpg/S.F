ced01f3f740db756b6f55e9726f1617f75685749
shipufalia-jpg <shipufalia@gmail.com>
2026-05-29 12:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    app.config["MAX_CONTENT_LENGTH"] = 500 * 1024 * 1024
+
     # ================= LOGIN MANAGER =================
     login_manager.init_app(app)
 
