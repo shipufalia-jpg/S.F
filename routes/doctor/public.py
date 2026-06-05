6e49334a67149d5208f6078ef526a2762afa6deb
shipufalia-jpg <shipufalia@gmail.com>
2026-06-05 15:47:20 +0530

    Create public.py

---
+++ routes/doctor/public.py
@@ -0,0 +1,14 @@
+from . import doctor_bp
+from models import Doctor
+from flask import render_template
+
+
+@doctor_bp.route("/")
+def doctor_list():
+
+    doctors = Doctor.query.order_by(Doctor.id.desc()).all()
+
+    return render_template(
+        "doctor/list.html",
+        doctors=doctors
+    )
