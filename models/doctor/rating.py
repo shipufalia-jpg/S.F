636b54405e702b1d129e242cf9df6fd7d99481f2
shipufalia-jpg <shipufalia@gmail.com>
2026-06-05 15:36:29 +0530

    Create rating.py

---
+++ models/doctor/rating.py
@@ -0,0 +1,22 @@
+from extensions import db
+from datetime import datetime
+
+
+class DoctorRating(db.Model):
+
+    __tablename__ = "doctor_ratings"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    doctor_id = db.Column(
+        db.Integer,
+        db.ForeignKey("doctors.id"),
+        nullable=False,
+        index=True
+    )
+
+    rating = db.Column(db.Integer, nullable=False)
+
+    ip_address = db.Column(db.String(50), index=True)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
