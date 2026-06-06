0bf96a6434c7d1b1c5032040aba79c3341621733
shipufalia-jpg <shipufalia@gmail.com>
2026-06-06 13:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -227,6 +227,8 @@ with app.app_context():
 
 # ================= RUN =================
 if __name__ == "__main__":
+     for rule in app.url_map.iter_rules():
+        print(rule)
     socketio.run(
         app,
         host="0.0.0.0",
