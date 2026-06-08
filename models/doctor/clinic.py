f94807a2481442a47034b5c62eef834ef0961d55
shipufalia-jpg <shipufalia@gmail.com>
2026-06-08 23:21:13 +0530

    Update clinic.py

--- models/doctor/clinic.py
+++ models/doctor/clinic.py
@@ -16,23 +16,21 @@ class Clinic(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
 
     phone = db.Column(db.String(20))
-
     address = db.Column(db.Text)
 
     status = db.Column(db.String(20), default="active")
 
+    # OWNER STRUCTURE
     created_by_admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-
     controller_admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
-    last_login = db.Column(db.DateTime)
+    # SUPER STRUCTURE
+    super_admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # =========================
-    # PASSWORD HELPERS
-    # =========================
-
+    # PASSWORD
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
