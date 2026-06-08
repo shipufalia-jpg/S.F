b1a215c237de59a53e6af5c8df6aa34aa60e1173
shipufalia-jpg <shipufalia@gmail.com>
2026-06-08 22:52:38 +0530

    Update chamber_permissions.py

--- utils/chamber_permissions.py
+++ utils/chamber_permissions.py
@@ -14,3 +14,26 @@ def chamber_required(f):
         return f(*args, **kwargs)
 
     return wrapper
+
+def can_create_chamber(user):
+    return user.role in ["owner", "super_admin", "admin"]
+
+
+def can_view_chamber(user, chamber):
+    if user.role == "owner":
+        return True
+
+    if user.role == "super_admin":
+        return chamber.super_admin_id == user.id
+
+    if user.role == "admin":
+        return chamber.admin_id == user.id
+
+    if user.role == "chamber":
+        return chamber.id == user.id
+
+    return False
+
+
+def can_block_or_delete(user):
+    return user.role == "owner"
