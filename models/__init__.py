821a4b7ed1666806db3cba972c125ea9a3a4506d
shipufalia-jpg <shipufalia@gmail.com>
2026-06-09 12:56:30 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@ from .user import User
 from .booking import Booking
 from models.profile import Profile
 from models.work_model import Work
-from .chamber import Chamber
+
 from .doctor.doctor import Doctor
 from .doctor.schedule import DoctorSchedule
 from .appointment import Appointment
