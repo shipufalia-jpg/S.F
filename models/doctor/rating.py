3673d57c1fcc0cf9516da97ded3582c0a033fc8a
shipufalia-jpg <shipufalia@gmail.com>
2026-06-13 12:54:13 +0530

    Update rating.py

--- models/doctor/rating.py
+++ models/doctor/rating.py
@@ -1,21 +1,34 @@
 from extensions import db
 from datetime import datetime
 
+
 class DoctorRating(db.Model):
 
-__tablename__ = "doctor_ratings"  
+    __tablename__ = "doctor_ratings"
 
-id = db.Column(db.Integer, primary_key=True)  
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
 
-doctor_id = db.Column(  
-    db.Integer,  
-    db.ForeignKey("doctors.id"),  
-    nullable=False,  
-    index=True  
-)  
+    doctor_id = db.Column(
+        db.Integer,
+        db.ForeignKey("doctors.id"),
+        nullable=False,
+        index=True
+    )
 
-rating = db.Column(db.Integer, nullable=False)  
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
 
-ip_address = db.Column(db.String(50), index=True)  
+    ip_address = db.Column(
+        db.String(50),
+        index=True
+    )
 
-created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
