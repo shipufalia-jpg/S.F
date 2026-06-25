e623db5769c0596a2297f4d2b7eba475d2eccade
shipufalia-jpg <shipufalia@gmail.com>
2026-06-25 19:30:20 +0530

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,12 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import os
 
 db = SQLAlchemy()
 
 socketio = SocketIO(
     cors_allowed_origins=os.environ.get("CORS_ORIGINS", "*"),
-    async_mode="threading",   # ✅ FIXED (eventlet remove)
+    async_mode="threading",
     ping_timeout=20,
     ping_interval=25
+)
+
+limiter = Limiter(
+    key_func=get_remote_address
 )
